48eb4eeb405684ee4be1c2f97716b2582303a464
Sam King <belven000@gmail.com>
2021-07-27 20:08:09 +0000

    Got Google docs link working

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ import discord
 import requests
 import json
 import re
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 from replit import db
 
+# define the scope
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+# add credentials to the account
+creds = ServiceAccountCredentials.from_json_keyfile_name('foxhole-data-365f75e26908.json', scope)
+
+# authorize the clientsheet 
+client_gd = gspread.authorize(creds)
+
 client = discord.Client()
+
 
 example_array = ['A', 'b', 'c']
 
@@ -13,6 +26,7 @@ amount_id = 3
 item_id = 2
 location_id = 1
 
+database_name = "requests"
 
 def get_data():
     response = requests.get("https://zenquotes.io/api/random")
@@ -26,7 +40,7 @@ def add_request(request):
     requests = None
 
     if check_requests_database():
-        requests = db["requests"]
+        requests = db[database_name]
         existing_request = check_existing_request(split_request)
 
         if existing_request is not None:
@@ -41,7 +55,7 @@ def add_request(request):
     else:
         database_request = create_database_request(split_request)
         requests = database_request
-        db["requests"] = create_database_request(split_request)
+        db[database_name] = create_database_request(split_request)
 
 
 def create_database_request(split_request):
@@ -54,7 +68,7 @@ def create_database_request(split_request):
 
 def find_request(location, item):
     if check_requests_database():
-        requests = db["requests"]
+        requests = db[database_name]
 
         for request in requests:
             data = requests[request]
@@ -66,7 +80,7 @@ def find_request(location, item):
 
 
 def check_requests_database():
-    return "requests" in db.keys() and db["requests"] is not None
+    return database_name in db.keys() and db[database_name] is not None
 
 
 def slipt_request(request):
@@ -98,8 +112,8 @@ def combine_request(split_request):
 
 #Takes in split request !request location item amount
 def check_existing_request(new_request):
-    if "requests" in db.keys():
-        requests = db["requests"]
+    if database_name in db.keys():
+        requests = db[database_name]
         location = get_request_location(new_request)
         item = get_request_item(new_request)
 
@@ -111,18 +125,40 @@ def check_existing_request(new_request):
 
 
 def delete_request(index):
-    requests = db["requests"]
+    requests = db[database_name]
     if len(requests) > index:
         del requests[index]
-        db["requests"] = requests
+        db[database_name] = requests
 
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    if check_requests_database():
-        db["requests"].clear()
 
+    sheet = client_gd.open('Test Data')
+
+    # get the first sheet of the Spreadsheet
+    sheet_instance = sheet.get_worksheet(0)
+
+    val = sheet_instance.cell(col=3,row=2)
+
+    print(val)
+
+    #get_values(me, sheet_id, range_name)
+    if check_requests_database():
+        db[database_name].clear()
+
+
+def get_values(self, spreadsheet_id, range_name):
+    service = self
+    
+    # [START sheets_get_values]
+    result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id,
+                                                 range=range_name).execute()
+    rows = result.get('values', [])
+    print('{0} rows retrieved.'.format(len(rows)))
+    # [END sheets_get_values]
+    return result
 
 @client.event
 async def on_message(message):
