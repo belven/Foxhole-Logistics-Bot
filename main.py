b9793359e5863d5c9c91ead4a42fb8eca1a4662b
Sam King <belven000@gmail.com>
2021-07-28 09:28:16 +0000

    Bot now reads from correct sheet and outputs on a loop

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import os
 import discord
-import requests
-import json
-import re
 import gspread
+from discord.ext import tasks
 from oauth2client.service_account import ServiceAccountCredentials
-
-from replit import db
 
 # define the scope
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
@@ -19,146 +15,64 @@ client_gd = gspread.authorize(creds)
 
 client = discord.Client()
 
+channel_id = ''
 
-example_array = ['A', 'b', 'c']
+def report_data(channel):
+    table_data = None
+    #sheet = client_gd.open('Test Data')
+    sheet = client_gd.open('SHIELD STOCKPILES')
+    sheet_instance = sheet.get_worksheet(2)
+    records_data = sheet_instance.get_all_records()
 
-amount_id = 3
-item_id = 2
-location_id = 1
+    for data_row in records_data:
+        name = data_row['Name']
+        amount_needed = data_row['Amount Needed']
+        importance = data_row['Importance']
 
-database_name = "requests"
+        if amount_needed > 0:
+            if importance is None:
+                importance = ''
 
-def get_data():
-    response = requests.get("https://zenquotes.io/api/random")
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + " -" + json_data[0]['a']
-    return (quote)
+            if name is None:
+                name = ''
 
+            if amount_needed is None:
+                amount_needed = ''
 
-def add_request(request):
-    split_request = slipt_request(request)
-    requests = None
+            data_row = str(importance) + ' ' + name + ' ' + str(amount_needed)
 
-    if check_requests_database():
-        requests = db[database_name]
-        existing_request = check_existing_request(split_request)
-
-        if existing_request is not None:
-            split_existing = slipt_request(existing_request)
-            val = get_request_amount(split_request) + get_request_amount(
-                split_existing)
-            split_existing[amount_id] = val
-            requests.append(combine_request(split_request))
-
-        else:
-            requests.append(request)
-    else:
-        database_request = create_database_request(split_request)
-        requests = database_request
-        db[database_name] = create_database_request(split_request)
+            if table_data is None:
+                table_data = [data_row]
+            else:
+                table_data.append(data_row)
+    
+    return table_data
 
 
-def create_database_request(split_request):
-    combined_request = combine_request(split_request)
-    return [
-        get_request_location(split_request) + '_' +
-        get_request_item(split_request), combined_request
-    ]
-
-
-def find_request(location, item):
-    if check_requests_database():
-        requests = db[database_name]
-
-        for request in requests:
-            data = requests[request]
-
-            if location in data and item in data:
-                print(request)
-                return request
-    return None
-
-
-def check_requests_database():
-    return database_name in db.keys() and db[database_name] is not None
-
-
-def slipt_request(request):
-    return re.findall(r'\S+', request)
-
-
-#Takes in split request !request location item amount
-def get_request_item(split_request):
-    return split_request[item_id]
-
-
-#Takes in split request !request location item amount
-def get_request_amount(split_request):
-    return split_request[amount_id]
-
-
-#Takes in split request !request location item amount
-def get_request_location(split_request):
-    return split_request[location_id]
-
-
-#Takes in split request !request location item amount
-def combine_request(split_request):
-    complete_request = get_request_location(
-        split_request) + ' ' + get_request_item(
-            split_request) + ' ' + get_request_amount(split_request)
-    return complete_request
-
-
-#Takes in split request !request location item amount
-def check_existing_request(new_request):
-    if database_name in db.keys():
-        requests = db[database_name]
-        location = get_request_location(new_request)
-        item = get_request_item(new_request)
-
-        id = find_request(location, item)
-
-        if id is not None:
-            return requests[id]
-    return None
-
-
-def delete_request(index):
-    requests = db[database_name]
-    if len(requests) > index:
-        del requests[index]
-        db[database_name] = requests
-
-
+def get_server_channel_id(server_in, channel_in):
+    for server in client.guilds:
+      if server.name == server_in:
+         for channel in server.channels:
+            if channel.name == channel_in:
+                return channel.id     
+    return ''   
+    
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    report_data_loop.start()
 
-    sheet = client_gd.open('Test Data')
+@tasks.loop(seconds=30.0)
+async def report_data_loop():
+  channel_id = get_server_channel_id("Belven's Gaming", "bot-test")
+  channel = client.get_channel(channel_id)
+  data = report_data(channel)
+  message_data = '\n'
 
-    # get the first sheet of the Spreadsheet
-    sheet_instance = sheet.get_worksheet(0)
+  for md in data:
+    message_data = message_data + md + '\n'
 
-    val = sheet_instance.cell(col=3,row=2)
-
-    print(val)
-
-    #get_values(me, sheet_id, range_name)
-    if check_requests_database():
-        db[database_name].clear()
-
-
-def get_values(self, spreadsheet_id, range_name):
-    service = self
-    
-    # [START sheets_get_values]
-    result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id,
-                                                 range=range_name).execute()
-    rows = result.get('values', [])
-    print('{0} rows retrieved.'.format(len(rows)))
-    # [END sheets_get_values]
-    return result
+  await channel.send(message_data)
 
 @client.event
 async def on_message(message):
@@ -166,10 +80,15 @@ async def on_message(message):
         return
     if message.content.startswith('!test'):
         await message.channel.send('Do not test me!')
-    if message.content.startswith('!quote'):
-        await message.channel.send(get_data())
-    if message.content.startswith('!request'):
-        add_request(message.content)
+    if message.content.startswith('!report_stock'):
+        data = report_data(message.channel)
+        message_data = '\n'
+
+        for md in data:
+          message_data = message_data + md + '\n'
+
+
+        await message.channel.send(message_data)
 
 
 my_secret = os.environ['token']
