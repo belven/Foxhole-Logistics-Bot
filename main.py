b1bd41ab36b34839e0f7b74740f794b9cfea8b32
Sam King <belven000@gmail.com>
2021-07-30 07:22:57 +0000

    Got bot fully working and using Pebblehost

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ client_gd = gspread.authorize(creds)
 
 client = discord.Client()
 
-channel_id = ''
+servers = { { 'server' : "S.H.I.E.L.D.", 'channel' : "shield-clan-chat"} }
 
 def report_data(channel):
     table_data = None
-    #sheet = client_gd.open('Test Data')
     sheet = client_gd.open('SHIELD STOCKPILES')
     sheet_instance = sheet.get_worksheet(2)
     records_data = sheet_instance.get_all_records()
@@ -27,19 +26,15 @@ def report_data(channel):
     for data_row in records_data:
         name = data_row['Name']
         amount_needed = data_row['Amount Needed']
-        importance = data_row['Importance']
 
         if amount_needed > 0:
-            if importance is None:
-                importance = ''
-
             if name is None:
                 name = ''
 
             if amount_needed is None:
                 amount_needed = ''
 
-            data_row = str(importance) + ' ' + name + ' ' + str(amount_needed)
+            data_row = name + ' ' + str(amount_needed)
 
             if table_data is None:
                 table_data = [data_row]
@@ -57,14 +52,17 @@ def get_server_channel_id(server_in, channel_in):
                 return channel.id     
     return ''   
     
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     report_data_loop.start()
 
-@tasks.loop(seconds=30.0)
+
+# Every 6 hours
+@tasks.loop(seconds=21600.0)
 async def report_data_loop():
-  channel_id = get_server_channel_id("Belven's Gaming", "bot-test")
+  channel_id = get_server_channel_id("S.H.I.E.L.D.", "shield-clan-chat")
   channel = client.get_channel(channel_id)
   data = report_data(channel)
   message_data = '\n'
@@ -73,6 +71,7 @@ async def report_data_loop():
     message_data = message_data + md + '\n'
 
   await channel.send(message_data)
+
 
 @client.event
 async def on_message(message):
@@ -87,10 +86,9 @@ async def on_message(message):
         for md in data:
           message_data = message_data + md + '\n'
 
-
         await message.channel.send(message_data)
 
 
-my_secret = os.environ['token']
+my_secret = 'ODY4ODI0NjIzNDk0NTk0NTkw.YP1R_A.8wthZjnflM9Ch4rYCJDPKvJsGQA'
 
 client.run(my_secret)
